444d96faca75be51a75c930c8b6eda9d90b5fd9a
Phillip Malboeuf <phil@boeuf.coffee>
2017-04-20 17:11:42 -0400

    config

--- core/__init__.py
+++ core/__init__.py
@@ -4,10 +4,10 @@ from flask_pymongo import PyMongo
 
 
 
-app = Flask(__name__, static_folder='build')
+app = Flask(__name__, static_folder='../build')
 
 
-# app.config.from_object('config.environment_default')
+app.config.from_object('config')
 
 # try:
 # 	app.config.from_object('config.environment_dev')
@@ -15,7 +15,7 @@ app = Flask(__name__, static_folder='build')
 # 	pass
 
 
-app.mongo = PyMongo(app)
+# app.mongo = PyMongo(app)
 
 from core.helpers.verify_headers import *
 from core.helpers.access_control_origin import *
